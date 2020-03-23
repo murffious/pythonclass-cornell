da4abfac3af0f4706c1c4afcf1ff978aa41e765c
murffious <titan.murff@gmail.com>
2020-03-23 11:27:36 -0600

    final project

---
+++ coursework/auditing-datasets/auditor/tests/__main__.py
@@ -0,0 +1,10 @@
+"""
+The entry point for unit test script
+
+This file imports the tests and runs them all on execution.
+
+Author: Walker M. White (wmw2)
+Date:   June 7, 2019
+"""
+from test_all import test
+test()
